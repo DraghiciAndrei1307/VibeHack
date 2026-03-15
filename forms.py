3f31a425c45e4c2b87104a316c211db3c71db2f6
Drăghici Andrei <draghici.andrei12@yahoo.com>
2026-03-15 12:34:35 +0200

    fix: remove ckeditor

--- forms.py
+++ forms.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, FloatField, BooleanField
 from wtforms.validators import DataRequired, URL, Email
-from flask_ckeditor import CKEditorField
 
 # RegisterForm to register new users
 
